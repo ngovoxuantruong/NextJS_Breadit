a644bc58cd9fd12f2e69f239fbdcfbd78500c6b3
Ngô Võ Xuân Trường <ngovoxuantruong8@gmail.com>
2023-08-14 16:23:27 +0700

    Voting & infinite scrolling

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -5,6 +5,9 @@ import { Post, User, Vote } from "@prisma/client";
 import { MessageSquare } from "lucide-react";
 import { FC, useRef } from "react";
 import EditorOutput from "./EditorOutput";
+import PostVoteClient from "./post-vote/PostVoteClient";
+
+type PartialVote = Pick<Vote, "type">;
 
 interface PostProps {
     subredditName: string;
@@ -13,16 +16,21 @@ interface PostProps {
         votes: Vote[];
     };
     commentAmount: number;
+    votesAmount: number;
+    currentVote?: PartialVote;
 }
 
-const Post: FC<PostProps> = ({ subredditName, post, commentAmount }) => {
+const Post: FC<PostProps> = ({ subredditName, post, commentAmount, votesAmount, currentVote }) => {
     const pRef = useRef<HTMLDivElement>(null);
 
     return (
         <div className="rounded-md bg-white shadow">
             <div className="px-6 py-4 flex justify-between">
-                {/* TODO: PostVotes  */}
-
+                <PostVoteClient
+                    postId={post.id}
+                    initialVote={currentVote?.type}
+                    initialVotesAmount={votesAmount}
+                />
                 <div className="w-0 flex-1">
                     <div className="max-h-40 mt-1 text-xs text-gray-500">
                         {subredditName ? (
--- src/components/post-vote/PostVoteClient.tsx
+++ src/components/post-vote/PostVoteClient.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { useCustomToast } from "@/hooks/useCustomToast";
 import { VoteType } from "@prisma/client";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { Button } from "../ui/Button";
+import { ArrowBigDown, ArrowBigUp } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useMutation } from "@tanstack/react-query";
+import { PostVoteRequest } from "@/lib/validators/vote";
+import axios, { AxiosError } from "axios";
+import { usePrevious } from "@mantine/hooks";
+import { toast } from "@/hooks/use-toast";
 
 interface PostVoteClientProps {
     postId: string;
@@ -8,7 +19,80 @@ interface PostVoteClientProps {
 }
 
 const PostVoteClient: FC<PostVoteClientProps> = ({ postId, initialVotesAmount, initialVote }) => {
-    return <div></div>;
+    const { loginToast } = useCustomToast();
+    const [votesAmount, setVotesAmount] = useState<number>(initialVotesAmount);
+    const [currentVote, setCurrentVote] = useState(initialVote);
+    const prevVote = usePrevious(currentVote);
+
+    useEffect(() => {
+        setCurrentVote(initialVote);
+    }, [initialVote]);
+
+    const { mutate: vote } = useMutation({
+        mutationFn: async (voteType: VoteType) => {
+            const payload: PostVoteRequest = {
+                postId,
+                voteType,
+            };
+
+            await axios.patch("/api/subreddit/post/vote", payload);
+        },
+
+        onError: (error, voteType) => {
+            if (voteType === "UP") setVotesAmount((prev) => prev - 1);
+            else setVotesAmount((prev) => prev + 1);
+
+            // reset current vote
+            setCurrentVote(prevVote);
+
+            if (error instanceof AxiosError) {
+                if (error.response?.status === 401) {
+                    return loginToast();
+                }
+
+                return toast({
+                    title: "Something went wrong",
+                    description: "Your vote was not registered, please try again.",
+                    variant: "destructive",
+                });
+            }
+        },
+        onMutate: (type: VoteType) => {
+            if (currentVote === type) {
+                setCurrentVote(undefined);
+                if (type === "UP") {
+                    setVotesAmount((prev) => prev - 1);
+                } else if (type === "DOWN") setVotesAmount((prev) => prev + 1);
+            } else {
+                setCurrentVote(type);
+                if (type === "UP") {
+                    setVotesAmount((prev) => prev + (currentVote ? 2 : 1));
+                } else if (type === "DOWN") setVotesAmount((prev) => prev - (currentVote ? 2 : 1));
+            }
+        },
+    });
+
+    return (
+        <div className="flex sm:flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-4 sm:pb-0">
+            <Button onClick={() => vote("UP")} size="sm" variant="ghost" aria-label="upvote">
+                <ArrowBigUp
+                    className={cn("h-5 w-5 text-zinc-700", {
+                        "text-emerald-500 fill-emerald-500": currentVote === "UP",
+                    })}
+                />
+            </Button>
+
+            <p className="text-center py-2 font-medium text-sm text-zinc-900">{votesAmount}</p>
+
+            <Button onClick={() => vote("DOWN")} size="sm" variant="ghost" aria-label="downvote">
+                <ArrowBigDown
+                    className={cn("h-5 w-5 text-zinc-700", {
+                        "text-red-500 fill-red-500": currentVote === "DOWN",
+                    })}
+                />
+            </Button>
+        </div>
+    );
 };
 
 export default PostVoteClient;
