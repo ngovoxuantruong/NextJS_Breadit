4b4ff4adc17a9b669e8fb41054ad4b5e6cfdb101
Ngô Võ Xuân Trường <ngovoxuantruong8@gmail.com>
2023-08-17 11:58:41 +0700

    Comments / reply comments & Voting comments

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,3 +1,4 @@
+import CommentsSection from "@/components/CommentsSection";
 import EditorOutput from "@/components/EditorOutput";
 import PostVoteServer from "@/components/post-vote/PostVoteServer";
 import { buttonVariants } from "@/components/ui/Button";
@@ -69,6 +70,11 @@ const page = async ({ params }: PageProps) => {
                     </h1>
 
                     <EditorOutput content={post?.content ?? cachedPost.content} />
+
+                    <Suspense fallback={<Loader2 className="w-5 h-5 animate-spin text-zinc-500" />}>
+                        {/* @ts-expect-error server component */}
+                        <CommentsSection postId={post?.id ?? cachedPost.id} />
+                    </Suspense>
                 </div>
             </div>
         </>
