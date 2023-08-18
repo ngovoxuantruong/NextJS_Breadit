e0d939456ed8b07b6647a7e9f81a240660026329
Ngô Võ Xuân Trường <ngovoxuantruong8@gmail.com>
2023-08-18 13:16:33 +0700

    Search bar & change username

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { Session } from "next-auth";
-import { usePathname, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
+import { Image as ImageIcon, Link2 } from "lucide-react";
 import { FC } from "react";
 import UserAvatar from "./UserAvatar";
-import { Input } from "./ui/Input";
-import { Button } from "./ui/Button";
-import { ImageIcon, Link2 } from "lucide-react";
+import type { Session } from "next-auth";
+import { usePathname, useRouter } from "next/navigation";
 
 interface MiniCreatePostProps {
     session: Session | null;
@@ -17,7 +17,7 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
     const pathname = usePathname();
 
     return (
-        <li className="overflow-hidden rounded-md bg-white shadow">
+        <li className="overflow-hidden rounded-md bg-white shadow list-none">
             <div className="h-full px-6 py-4 flex justify-between gap-6">
                 <div className="relative">
                     <UserAvatar
@@ -27,20 +27,17 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
                         }}
                     />
 
-                    <span className="absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white" />
+                    <span className="absolute bottom-2 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white" />
                 </div>
-
                 <Input
-                    readOnly
                     onClick={() => router.push(pathname + "/submit")}
-                    placeholder="Create post "
+                    readOnly
+                    placeholder="Create post"
                 />
-
-                <Button variant="ghost" onClick={() => router.push(pathname + "/submit")}>
+                <Button onClick={() => router.push(pathname + "/submit")} variant="ghost">
                     <ImageIcon className="text-zinc-600" />
                 </Button>
-
-                <Button variant="ghost" onClick={() => router.push(pathname + "/submit")}>
+                <Button onClick={() => router.push(pathname + "/submit")} variant="ghost">
                     <Link2 className="text-zinc-600" />
                 </Button>
             </div>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -45,7 +45,7 @@ const Post: FC<PostProps> = ({ subredditName, post, commentAmount, votesAmount, 
                                 <span className="px-1">•</span>
                             </>
                         ) : null}
-                        <span>Posted by u/{post.author.name} </span>{" "}
+                        <span>Posted by u/{post.author.username} </span>{" "}
                         {formatTimeToNow(new Date(post.createdAt))}
                     </div>
 
